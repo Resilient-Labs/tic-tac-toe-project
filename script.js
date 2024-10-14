560e69c946d63b8957b28eb0feee63cc7c5e656d
sunny <ethsok@protonmail.com>
2024-10-13 21:04:29 -0400

    tictactoe

--- script.js
+++ script.js
@@ -56,10 +56,34 @@ function winLoseOrDraw(){
     
 
     console.log(cell1,cell2,cell3)
+
     if(cell1 === 'X' && cell2 === 'X' && cell3 === 'X' && cell1 !== ''){
         h1.innerHTML = 'We have a winner (X)'
-    }else if (cell1 === 'X' && cell2 === 'X' && cell3 === 'X' && cell1 !== ''){
-        
+    }else if (cell4 === 'X' && cell5 === 'X' && cell6 === 'X' && cell4 !== ''){
+         h1.innerHTML = 'We have a winner (X)'
+    }else if (cell7 === 'X' && cell8 === 'X' && cell9 === 'X' && cell7 !== ''){
+        h1.innerHTML = 'We have a winner (X)'
+    }else if (cell1 === 'X' && cell5 === 'X' && cell9 === 'X' && cell1 !== ''){
+         h1.innerHTML = 'We have a winner (X)'
+    }else if (cell7 === 'X' && cell5 === 'X' && cell3 === 'X' && cell7 !== ''){
+        h1.innerHTML = 'We have a winner (X)'
+    }
+    
+    
+    else if (cell4 === 'O' && cell5 === 'O' && cell6 === 'O' && cell4 !== ''){
+         h1.innerHTML = 'We have a winner (O)'
+    }else if (cell7 === 'O' && cell8 === 'O' && cell9 === 'O' && cell7 !== ''){
+        h1.innerHTML = 'We have a winner (O)'
+    }else if (cell1 === 'O' && cell5 === 'O' && cell9 === 'O' && cell1 !== ''){
+        h1.innerHTML = 'We have a winner (O)'
+    }else if (cell7 === 'O' && cell5 === 'O' && cell3 === 'O' && cell7 !== ''){
+        h1.innerHTML = 'We have a winner (O)'
+    }else if (cell1 === 'O' && cell2 === 'O' && cell3 === 'O' && cell1 !== ''){
+        h1.innerHTML = 'We have a winner (O)'
     }
 
-}
+    if(cell1 !== '' && cell2 !== '' && cell3 !== '' && cell4 !== '' && cell5 !== '' && cell6 !== '' && cell7 !== '' && cell8 !== '' && cell9 !== ''){
+        h1.innerHTML = 'Draw'
+    }
+
+    }
