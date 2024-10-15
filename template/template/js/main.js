9aa511f4cde46ee8de0ba857ad07ef34aa117bff
Cole W. <your.email@example.com>
2024-10-15 12:05:12 -0400

    completed the challenge

---
+++ template/template/js/main.js
@@ -0,0 +1,14 @@
+// need objects to keep track of different parts of the game 
+
+/*
+- game object
+        - player 1 chooses  X &  player 2 has O
+        - players alternate turns
+        - one player has to get three in a row
+        - can do in line up down or across or diagonal
+
+*/
+
+
+
+
